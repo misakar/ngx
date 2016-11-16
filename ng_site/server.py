8a3cc26304f2538daa7d931ac7ded46e0248a890
neo1218 <neo1218@yeah.net>
2016-11-16 18:45:22 +0800

    back to python3.4:(

--- ng_site/server.py
+++ ng_site/server.py
@@ -2,18 +2,22 @@ import json
 import asyncio
 import websockets
 from config import host, port
+from types import coroutine
 
 from config import nginx_log_file_path
 from tail_f import tail_f
 from handle import handle
 
-async def handle_server(websocket, path):
+# async def handle_server(websocket, path):
+@coroutine
+def handle_server(websocket, path):
     logfile = open(nginx_log_file_path)
     lines = tail_f(logfile)
     log_dicts = handle(lines)
 
     for log_dict in log_dicts:
-        await websocket.send(str(log_dict))
+        # await websocket.send(str(log_dict))
+        yield from websocket.send(str(log_dict))
 
 start_server = websockets.serve(handle_server, host, int(port))
 

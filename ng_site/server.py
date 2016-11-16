8d1cece8b929f03972f322118fb40a6fa4339a7e
neo1218 <neo1218@yeah.net>
2016-11-16 20:44:57 +0800

    add request per second

--- ng_site/server.py
+++ ng_site/server.py
@@ -16,16 +16,20 @@ def handle_server(websocket, path):
     lines = tail_f(logfile)
     log_dicts = handle(lines)
 
-    start = time.time()
     for log_dict in log_dicts:
         # await websocket.send(str(log_dict))
         request_count += 1
         during_time = time.time() - start
-        log_dict.update(
-            {'request_per_second': (request_count // during_time)}
-        )
-        yield from websocket.send(str(log_dict))
+        log_dict.update({
+            'request_per_second': round(
+                (request_count / during_time), 2
+            )
+        })
+        yield from websocket.send(str(
+            json.dumps(log_dict)
+        ))
 
+start = time.time()
 start_server = websockets.serve(handle_server, host, int(port))
 
 asyncio.get_event_loop().run_until_complete(start_server)
